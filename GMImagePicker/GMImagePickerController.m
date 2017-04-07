1fe7b9bceddb143fa1303c23035b761071628b1c
coybit <coybit@gmail.com>
2017-04-07 05:21:17 -0700

    Fixes a bug where it causes crash when you have already removed the navigation 1px wide subview

--- GMImagePicker/GMImagePickerController.m
+++ GMImagePicker/GMImagePickerController.m
@@ -86,7 +86,10 @@
     _navigationController.toolbar.translucent = YES;
     _navigationController.toolbar.barTintColor = _toolbarBarTintColor;
     _navigationController.toolbar.tintColor = _toolbarTintColor;
-    [(UIView*)[_navigationController.toolbar.subviews objectAtIndex:0] setAlpha:0.75f];  // URGH - I know!
+    
+    if( [_navigationController.toolbar.subviews count] >= 1 ) {
+        [(UIView*)[_navigationController.toolbar.subviews objectAtIndex:0] setAlpha:0.75f];  // URGH - I know!
+    }
     
     _navigationController.navigationBar.backgroundColor = _navigationBarBackgroundColor;
     _navigationController.navigationBar.tintColor = _navigationBarTintColor;
